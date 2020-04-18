5ffd1dc41c61779285618d41c9d9f89f3dc863e7
secretXIAOYU <734839069@qq.com>
2020-04-18 17:10:43 +0800

    format

--- demo-react/src/TodoListHeader.js
+++ demo-react/src/TodoListHeader.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
-const TodoListHeader = () => <div>
+const TodoListHeader = () => 
+    <div>
         <h2>Todo List</h2>
         <p><i>点击条目即可实现“完成”和“未完成”的转换（灰色背景为“已完成”）</i></p>
     </div>
